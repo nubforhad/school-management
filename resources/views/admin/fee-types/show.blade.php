d1b91096c7cc8ccb18fca10875fca0026ea5e40b
nubforhad <nub.forhad@gmail.com>
2026-08-23 15:20:08 +0600

    show fee types

---
+++ resources/views/admin/fee-types/show.blade.php
@@ -0,0 +1,10 @@
+@extends('admin.layouts.app')
+
+@section('content')
+
+<div class="max-w-3xl mx-auto px-3 sm:px-4 lg:px-6 py-4 sm:py-6">
+
+    <h4>Hiiiiiiiiiiiiiiiiiiiiiiiiiiii</h4>
+</div>
+
+@endsection
